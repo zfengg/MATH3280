74e26719931caa1629e1dc63103117261776aee3
Zhou FENG <42152221+zfengg@users.noreply.github.com>
2021-09-12 22:19:46 +0800

    add value checking

--- tutorials/T2/T2.jl
+++ tutorials/T2/T2.jl
@@ -1,5 +1,20 @@
 using Combinatorics
 
+# Example 3 formula values
+# (a) = 1//158753389900 = 6.299078089796431e-12
+pA = 4 // binomial(52, 13) |> Float64
+
+# (b) = 2197//20825 = 0.10549819927971188
+pB = (factorial(4) * multinomial(BigInt(12), BigInt(12), BigInt(12), BigInt(12))) // multinomial(BigInt(13), BigInt(13), BigInt(13), BigInt(13))
+pB = Float64(pB)
+
+# (c) = 316451606//333515525 = 0.9488362078496946..
+probUnionC = binomial(4, 1) * binomial(39, 13) // binomial(52, 13)
+			- binomial(4, 2) * binomial(39, 26) // binomial(52, 26)
+			+ binomial(4, 3) * binomial(39, 39) // binomial(52, 39)
+pC = 1 - probUnionC |> Float64
+
+
 # check the formula in Example 3(c)
 numCardPerColor = 4
 numColors = 2
@@ -16,7 +31,7 @@ function count_partition(numCardPerColor, numColors)
 		for j in 1:numColors
 			if sum(tmpPartition[j] .<= numCardPerColor) == 0
 				flag = false
-			end	
+			end
 		end
 		if flag
 			push!(container, Set.(tmpPartition))
@@ -27,9 +42,3 @@ function count_partition(numCardPerColor, numColors)
 end
 
 countTargets, allTargets = count_partition(numCardPerColor, numColors)
-
-# formula results in Example 3(c)
-probUnionC = binomial(4, 1) * binomial(39, 13) // binomial(52, 13) 
-			- binomial(4, 2) * binomial(39, 26) // binomial(52, 26) 
-			+ binomial(4, 3) * binomial(39, 39) // binomial(52, 39) 
-probAsLeast1Heart = 1 - probUnionC # 316451606//333515525 = 0.9488362078496946..

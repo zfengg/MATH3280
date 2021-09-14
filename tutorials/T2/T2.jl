6f2af4aa93f58f314bf2d071db5e8874422c34db
Zhou FENG <42152221+zfengg@users.noreply.github.com>
2021-09-14 09:51:48 +0800

    add T2.pdf

--- tutorials/T2/T2.jl
+++ tutorials/T2/T2.jl
@@ -14,31 +14,3 @@ probUnionC = binomial(4, 1) * binomial(39, 13) // binomial(52, 13)
 			+ binomial(4, 3) * binomial(39, 39) // binomial(52, 39)
 pC = 1 - probUnionC |> Float64
 
-
-# check the formula in Example 3(c)
-numCardPerColor = 4
-numColors = 2
-
-function count_partition(numCardPerColor, numColors)
-    numTotalCards = numCardPerColor * numColors
-    container = []
-    for perm in permutations(1:numTotalCards)
-		tmpPartition = []
-		for j in 1:numColors
-			push!(tmpPartition, perm[(j - 1) * numCardPerColor + 1:j * (numCardPerColor)])
-		end
-		flag = true
-		for j in 1:numColors
-			if sum(tmpPartition[j] .<= numCardPerColor) == 0
-				flag = false
-			end
-		end
-		if flag
-			push!(container, Set.(tmpPartition))
-			unique!(container)
-		end
-    end
-    return length(container), container
-end
-
-countTargets, allTargets = count_partition(numCardPerColor, numColors)
